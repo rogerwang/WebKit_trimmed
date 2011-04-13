rogerwang/WebKit_trimmed

commit 65f35e020b48afbb8dbcda075d1520cc613c4226

2011-04-12  Sergey Glazunov  <serg.glazunov@gmail.com>
        Reviewed by Dimitri Glazkov.

        Element::setAttribute should check the namespace of an id attribute
        https://bugs.webkit.org/show_bug.cgi?id=58359

        * fast/dom/id-attribute-with-namespace-crash-expected.txt: Added.
        * fast/dom/id-attribute-with-namespace-crash.html: Added.
2011-04-12  Sergey Glazunov  <serg.glazunov@gmail.com>

        Reviewed by Dimitri Glazkov.

        Element::setAttribute should check the namespace of an id attribute
        https://bugs.webkit.org/show_bug.cgi?id=58359

        Test: fast/dom/id-attribute-with-namespace-crash.html

        * dom/Element.cpp:
        (WebCore::Element::setAttribute):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@83699 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/fast/dom/id-attribute-with-namespace-crash-expected.txt
@@ -1,0 +1,1 @@
+PASS

LayoutTests/fast/dom/id-attribute-with-namespace-crash.html
@@ -1,0 +1,42 @@
+<html>
+<head>
+<script>
+if (window.layoutTestController) {
+    layoutTestController.dumpAsText();
+    layoutTestController.waitUntilDone();
+}
+
+gc = window.gc || function()
+{
+    if (window.GCController)
+        return GCController.collect();
+
+    for (var i = 0; i < 10000; ++i)
+        var s = new String("AAAA");
+}
+
+window.onload = function()
+{
+    element = document.body.appendChild(document.createElement("a"));
+
+    element.setAttributeNS("namespace", "id", "foo");
+    element.setAttribute("id", "bar");
+    
+    document.body.removeChild(element);
+    element = null;
+    gc();
+    
+    setTimeout(finishTest, 0);
+}
+
+finishTest = function()
+{
+    document.getElementById("bar");
+
+    if (window.layoutTestController)
+        layoutTestController.notifyDone();
+}
+</script>
+</head>
+<body>PASS</body>
+</html>

Source/WebCore/dom/Element.cpp
@@ -657,22 +657,20 @@ void Element::setAttribute(const AtomicString& name, const AtomicString& value, 
 #endif
 
     const AtomicString& localName = shouldIgnoreAttributeCase(this) ? name.lower() : name;
-
+    QualifiedName attributeName(nullAtom, localName, nullAtom);
+    
     // Allocate attribute map if necessary.
     Attribute* old = attributes(false)->getAttributeItem(localName, false);
 
     document()->incDOMTreeVersion();
 
-    // FIXME: This check is probably not correct for the case where the document has an id attribute
-    // with a non-null namespace, because it will return true if the local name happens to match
-    // but the namespace does not.
-    if (localName == document()->idAttributeName().localName())
+    if (isIdAttributeName(old ? old->name() : attributeName))
         updateId(old ? old->value() : nullAtom, value);
 
     if (old && value.isNull())
         m_attributeMap->removeAttribute(old->name());
     else if (!old && !value.isNull())
-        m_attributeMap->addAttribute(createAttribute(QualifiedName(nullAtom, localName, nullAtom), value));
+        m_attributeMap->addAttribute(createAttribute(attributeName, value));
     else if (old && !value.isNull()) {
         if (Attr* attrNode = old->attr())
             attrNode->setValue(value);

commit 1ece16a2074324aad8d9a2d0ebda839300da8d69

[Windows XP Debug Tests] fast/events/pageshow-pagehide-on-back-cached-with-frames.html failing since r83699 https://bugs.webkit.org/show_bug.cgi?id=58441
Add the Windows-XP specific expected failing results to get the bots green.

* platform/win-xp/fast/events: Added.
* platform/win-xp/fast/events/pageshow-pagehide-on-back-cached-with-frames-expected.txt: Added.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@83738 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/win-xp/fast/events/pageshow-pagehide-on-back-cached-with-frames-expected.txt
@@ -1,0 +1,27 @@
+CONSOLE MESSAGE: line 20: ***Top level frame being parsed for the initial page load***
+CONSOLE MESSAGE: line 20: Subsubframe window.onload
+CONSOLE MESSAGE: line 20: Subsubframe window.onpageshow, target = [object HTMLDocument], persisted = false
+CONSOLE MESSAGE: line 20: Subframe window.onload
+CONSOLE MESSAGE: line 20: Subframe window.onpageshow, target = [object HTMLDocument], persisted = false
+CONSOLE MESSAGE: line 20: Main frame window.onload
+CONSOLE MESSAGE: line 20: Main frame window.onpageshow, target = [object HTMLDocument], persisted = false
+CONSOLE MESSAGE: line 20: ***Navigating top-level frame to a page that will immediately navigate back to this one***
+CONSOLE MESSAGE: line 20: Main frame window.onpagehide, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Subframe window.onpagehide, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Subsubframe window.onpagehide, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: ***Top level frame being parsed for the initial page load***
+CONSOLE MESSAGE: line 20: Subsubframe window.onload
+CONSOLE MESSAGE: line 20: Subsubframe window.onpageshow, target = [object HTMLDocument], persisted = false
+CONSOLE MESSAGE: line 20: Subframe window.onload
+CONSOLE MESSAGE: line 20: Subframe window.onpageshow, target = [object HTMLDocument], persisted = false
+CONSOLE MESSAGE: line 20: Main frame window.onload
+CONSOLE MESSAGE: line 20: Main frame window.onpageshow, target = [object HTMLDocument], persisted = false
+CONSOLE MESSAGE: line 20: ***Navigating top-level frame to a page that will immediately navigate back to this one***
+CONSOLE MESSAGE: line 20: Main frame window.onpagehide, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Subframe window.onpagehide, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Subsubframe window.onpagehide, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Subsubframe window.onpageshow, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Subframe window.onpageshow, target = [object HTMLDocument], persisted = true
+CONSOLE MESSAGE: line 20: Main frame window.onpageshow, target = [object HTMLDocument], persisted = true
+Test pageshow/pagehide event behavior when navigating away from a page with frames, putting the page in the page cache, then back to it.
+