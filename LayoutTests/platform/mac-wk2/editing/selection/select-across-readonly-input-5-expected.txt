rogerwang/WebKit_trimmed

commit 8b428ced9350f3a242d92f9911e7f3798d467ac0

2011-05-03  Ryosuke Niwa  <rniwa@webkit.org>
        Reviewed by Darin Adler.

        WebKit allows selection that crosses the shadow boundary of a readonly input element
        https://bugs.webkit.org/show_bug.cgi?id=60000

        Added pixel tests to ensure WebKit doesn't create a selection that spans across
        the shadow DOM boundary of a readonly input element. Also rebaselined a test that
        previously exhibited this behavior.

        * editing/selection/resources/select-across-readonly-input.js: Added.
        (moveMouseToMiddleOfElement):
        * editing/selection/select-across-readonly-input-1.html: Added.
        * editing/selection/select-across-readonly-input-2.html: Added.
        * editing/selection/select-across-readonly-input-3.html: Added.
        * editing/selection/select-across-readonly-input-4.html: Added.
        * editing/selection/select-across-readonly-input-5.html: Added.
        * platform/mac/editing/selection/select-across-readonly-input-1-expected.png: Added.
        * platform/mac/editing/selection/select-across-readonly-input-1-expected.txt: Added.
        * platform/mac/editing/selection/select-across-readonly-input-2-expected.png: Added.
        * platform/mac/editing/selection/select-across-readonly-input-2-expected.txt: Added.
        * platform/mac/editing/selection/select-across-readonly-input-3-expected.png: Added.
        * platform/mac/editing/selection/select-across-readonly-input-3-expected.txt: Added.
        * platform/mac/editing/selection/select-across-readonly-input-4-expected.png: Added.
        * platform/mac/editing/selection/select-across-readonly-input-4-expected.txt: Added.
        * platform/mac/editing/selection/select-across-readonly-input-5-expected.png: Added.
        * platform/mac/editing/selection/select-across-readonly-input-5-expected.txt: Added.
        * platform/mac/fast/forms/input-readonly-autoscroll-expected.txt:
2011-05-03  Ryosuke Niwa  <rniwa@webkit.org>

LayoutTests/platform/mac/editing/selection/select-across-readonly-input-1-expected.txt
@@ -1,0 +1,27 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 774x36
+          text run at (0,0) width 774: "To manually test, select text by a mouse drag starting in \"hello\" and ending in \"world\". Selection should only extend inside"
+          text run at (0,18) width 112: "the input element."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+selection start: position 3 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 5 {DIV} of body
+selection end:   position 5 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 5 {DIV} of body

LayoutTests/platform/mac/editing/selection/select-across-readonly-input-2-expected.txt
@@ -1,0 +1,27 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "To manually test, select text by a mouse drag starting in \"world\" and ending in \"hello\". Selection should not extend into the"
+          text run at (0,18) width 89: "input element."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+selection start: position 0 of child 2 {#text} of child 5 {DIV} of body
+selection end:   position 2 of child 0 {#text} of child 3 {SPAN} of child 5 {DIV} of body

LayoutTests/platform/mac/editing/selection/select-across-readonly-input-3-expected.txt
@@ -1,0 +1,27 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "To manually test, select text by a mouse drag starting in \"hello\" and ending in \"world\". Selection should not extend into the"
+          text run at (0,18) width 89: "input element."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderInline {SPAN} at (0,0) size 31x18
+          RenderText {#text} at (0,6) size 31x18
+            text run at (0,6) width 31: "hello"
+        RenderText {#text} at (31,6) size 4x18
+          text run at (31,6) width 4: " "
+        RenderTextControl {INPUT} at (35,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+layer at (46,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 44x18
+      text run at (1,0) width 44: "world"
+selection start: position 2 of child 0 {#text} of child 1 {SPAN} of child 5 {DIV} of body
+selection end:   position 1 of child 2 {#text} of child 5 {DIV} of body

LayoutTests/platform/mac/editing/selection/select-across-readonly-input-4-expected.txt
@@ -1,0 +1,34 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 762x36
+          text run at (0,0) width 762: "To manually test, select text by a mouse drag starting in \"hello\" and ending in \"WebKit\". Selection should extend only in"
+          text run at (0,18) width 244: "the input element that contains \"hello\"."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (100,6) size 4x18
+          text run at (100,6) width 4: " "
+        RenderTextControl {INPUT} at (104,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+layer at (115,117) size 53x18 scrollWidth 56
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 54x18
+      text run at (1,0) width 54: "WebKit"
+selection start: position 3 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 5 {DIV} of body
+selection end:   position 5 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 5 {DIV} of body

LayoutTests/platform/mac/editing/selection/select-across-readonly-input-5-expected.txt
@@ -1,0 +1,34 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 762x36
+          text run at (0,0) width 762: "To manually test, select text by a mouse drag starting in \"WebKit\" and ending in \"hello\". Selection should extend only in"
+          text run at (0,18) width 263: "the input element that contains \"WebKit\"."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (100,6) size 4x18
+          text run at (100,6) width 4: " "
+        RenderTextControl {INPUT} at (104,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+layer at (115,117) size 53x18 scrollWidth 56
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 54x18
+      text run at (1,0) width 54: "WebKit"
+selection start: position 0 of child 0 {#text} of child 0 {DIV} of child 5 {INPUT} of child 5 {DIV} of body
+selection end:   position 2 of child 0 {#text} of child 0 {DIV} of child 5 {INPUT} of child 5 {DIV} of body

LayoutTests/editing/selection/resources/select-across-readonly-input.js
@@ -1,0 +1,26 @@
+
+function moveMouseToMiddleOfElement(element)
+{
+    eventSender.mouseMoveTo(element.offsetLeft + element.offsetWidth / 2, element.offsetTop + element.offsetHeight / 2);
+}
+
+var inputs = document.getElementsByTagName('input');
+for (var i = 0; i < inputs.length; i++)
+{
+    inputs[i].style.fontSize = '1em';
+    inputs[i].style.width = '7ex';
+}
+
+if (window.layoutTestController) {
+    if (!window.eventSender)
+        document.writeln('This test requires eventSender');
+    else {
+        moveMouseToMiddleOfElement(document.getElementById('from'));
+        eventSender.mouseDown();
+
+        eventSender.leapForward(200);
+
+        moveMouseToMiddleOfElement(document.getElementById('to'));
+        eventSender.mouseUp();
+    }
+}

LayoutTests/editing/selection/select-across-readonly-input-1.html
@@ -1,0 +1,12 @@
+<!DOCTYPE>
+<html>
+<body>
+<p>This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse drag.</p>
+<p>To manually test, select text by a mouse drag starting in "hello" and ending in "world". Selection should only extend inside the input element.</p>
+<div id="test">
+<input id="from" type="text" value="hello" readonly>
+<span id="to">world</span>
+</div>
+<script src="resources/select-across-readonly-input.js"></script>
+</body>
+</html>

LayoutTests/editing/selection/select-across-readonly-input-2.html
@@ -1,0 +1,12 @@
+<!DOCTYPE>
+<html>
+<body>
+<p>This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse drag.</p>
+<p>To manually test, select text by a mouse drag starting in "world" and ending in "hello". Selection should not extend into the input element.</p>
+<div id="test">
+<input id="to" type="text" value="hello" readonly>
+<span id="from">world</span>
+</div>
+<script src="resources/select-across-readonly-input.js"></script>
+</body>
+</html>

commit 07e5eef02228e1492b5cd56875b697f52512fc7b

[WebKit2] editing/selection/select-across-readonly-input-{1-5}.html failing since introduction in r85669. https://bugs.webkit.org/show_bug.cgi?id=60272
Add expected failing text results to get the bots green.

Note: these tests still need pixel results to be generated both for mac-wk2 and win-wk2.

* platform/mac-wk2/editing/selection: Added.
* platform/mac-wk2/editing/selection/select-across-readonly-input-1-expected.txt: Added.
* platform/mac-wk2/editing/selection/select-across-readonly-input-2-expected.txt: Added.
* platform/mac-wk2/editing/selection/select-across-readonly-input-3-expected.txt: Added.
* platform/mac-wk2/editing/selection/select-across-readonly-input-4-expected.txt: Added.
* platform/mac-wk2/editing/selection/select-across-readonly-input-5-expected.txt: Added.



git-svn-id: http://svn.webkit.org/repository/webkit/trunk@86011 268f45cc-cd09-0410-ab3c-d52691b4dbfc

LayoutTests/platform/mac-wk2/editing/selection/select-across-readonly-input-1-expected.txt
@@ -1,0 +1,26 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 774x36
+          text run at (0,0) width 774: "To manually test, select text by a mouse drag starting in \"hello\" and ending in \"world\". Selection should only extend inside"
+          text run at (0,18) width 112: "the input element."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+caret: position 3 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 5 {DIV} of body

LayoutTests/platform/mac-wk2/editing/selection/select-across-readonly-input-2-expected.txt
@@ -1,0 +1,26 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "To manually test, select text by a mouse drag starting in \"world\" and ending in \"hello\". Selection should not extend into the"
+          text run at (0,18) width 89: "input element."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+caret: position 2 of child 0 {#text} of child 3 {SPAN} of child 5 {DIV} of body

LayoutTests/platform/mac-wk2/editing/selection/select-across-readonly-input-3-expected.txt
@@ -1,0 +1,26 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "To manually test, select text by a mouse drag starting in \"hello\" and ending in \"world\". Selection should not extend into the"
+          text run at (0,18) width 89: "input element."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderInline {SPAN} at (0,0) size 31x18
+          RenderText {#text} at (0,6) size 31x18
+            text run at (0,6) width 31: "hello"
+        RenderText {#text} at (31,6) size 4x18
+          text run at (31,6) width 4: " "
+        RenderTextControl {INPUT} at (35,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+layer at (46,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 44x18
+      text run at (1,0) width 44: "world"
+caret: position 2 of child 0 {#text} of child 1 {SPAN} of child 5 {DIV} of body

LayoutTests/platform/mac-wk2/editing/selection/select-across-readonly-input-4-expected.txt
@@ -1,0 +1,33 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 762x36
+          text run at (0,0) width 762: "To manually test, select text by a mouse drag starting in \"hello\" and ending in \"WebKit\". Selection should extend only in"
+          text run at (0,18) width 244: "the input element that contains \"hello\"."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (100,6) size 4x18
+          text run at (100,6) width 4: " "
+        RenderTextControl {INPUT} at (104,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+layer at (115,117) size 53x18 scrollWidth 56
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 54x18
+      text run at (1,0) width 54: "WebKit"
+caret: position 3 of child 0 {#text} of child 0 {DIV} of child 1 {INPUT} of child 5 {DIV} of body

LayoutTests/platform/mac-wk2/editing/selection/select-across-readonly-input-5-expected.txt
@@ -1,0 +1,33 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {P} at (0,0) size 784x36
+        RenderText {#text} at (0,0) size 776x36
+          text run at (0,0) width 776: "This test ensures selection that crosses the shadow DOM boundary of a readonly input element cannot be made by a mouse"
+          text run at (0,18) width 32: "drag."
+      RenderBlock {P} at (0,52) size 784x36
+        RenderText {#text} at (0,0) size 762x36
+          text run at (0,0) width 762: "To manually test, select text by a mouse drag starting in \"WebKit\" and ending in \"hello\". Selection should extend only in"
+          text run at (0,18) width 263: "the input element that contains \"WebKit\"."
+      RenderBlock {DIV} at (0,104) size 784x28
+        RenderTextControl {INPUT} at (0,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (59,6) size 4x18
+          text run at (59,6) width 4: " "
+        RenderInline {SPAN} at (0,0) size 37x18
+          RenderText {#text} at (63,6) size 37x18
+            text run at (63,6) width 37: "world"
+        RenderText {#text} at (100,6) size 4x18
+          text run at (100,6) width 4: " "
+        RenderTextControl {INPUT} at (104,2) size 59x24 [bgcolor=#FFFFFF] [border: (2px inset #000000)]
+        RenderText {#text} at (0,0) size 0x0
+layer at (11,117) size 53x18
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 38x18
+      text run at (1,0) width 38: "hello"
+layer at (115,117) size 53x18 scrollWidth 56
+  RenderBlock {DIV} at (3,3) size 53x18
+    RenderText {#text} at (1,0) size 54x18
+      text run at (1,0) width 54: "WebKit"
+caret: position 2 of child 0 {#text} of child 0 {DIV} of child 5 {INPUT} of child 5 {DIV} of body