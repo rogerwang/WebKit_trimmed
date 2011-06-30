rogerwang/WebKit_trimmed

commit 9e09a5fcf3dc3fda3d0210157738f17509b7fd1c

Make LayoutTestResultsLoader cache whether old-run-webkit-tests exited early due to too many failures
Fixes <http://webkit.org/b/63470> TestFailures page for a particular builder forgets
old-run-webkit-tests exited early after reload

Reviewed by Anders Carlsson.

* BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/LayoutTestResultsLoader.js:
(LayoutTestResultsLoader.prototype.start): Store both the set of failing tests and whether
old-run-webkit-tests exited early due to too many failures in PersistentCache.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@89841 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/LayoutTestResultsLoader.js
@@ -31,11 +31,15 @@ LayoutTestResultsLoader.prototype = {
     start: function(buildName, callback, errorCallback) {
         var cacheKey = 'LayoutTestResultsLoader.' + this._builder.name + '.' + buildName;
         if (PersistentCache.contains(cacheKey)) {
-            callback(PersistentCache.get(cacheKey));
-            return;
+            var cachedData = PersistentCache.get(cacheKey);
+            // Old versions of this function used to cache only the set of tests.
+            if ('tooManyFailures' in cachedData) {
+                callback(cachedData.tests, cachedData.tooManyFailures);
+                return;
+            }
         }
 
-        var tests = {};
+        var result = { tests: {}, tooManyFailures: false };
 
         var parsedBuildName = this._builder.buildbot.parseBuildName(buildName);
 
@@ -50,7 +54,7 @@ LayoutTestResultsLoader.prototype = {
                 root.innerHTML = xhr.responseText;
 
                 if (resultsHTMLSupportsTooManyFailuresInfo)
-                    tooManyFailures = root.getElementsByClassName('stopped-running-early-message').length > 0;
+                    result.tooManyFailures = root.getElementsByClassName('stopped-running-early-message').length > 0;
 
                 function testsForResultTable(regex) {
                     var paragraph = Array.prototype.findFirst.call(root.querySelectorAll('p'), function(paragraph) {
@@ -66,25 +70,25 @@ LayoutTestResultsLoader.prototype = {
                 }
 
                 testsForResultTable(/did not match expected results/).forEach(function(name) {
-                    tests[name] = 'fail';
+                    result.tests[name] = 'fail';
                 });
                 testsForResultTable(/timed out/).forEach(function(name) {
-                    tests[name] = 'timeout';
+                    result.tests[name] = 'timeout';
                 });
                 testsForResultTable(/tool to crash/).forEach(function(name) {
-                    tests[name] = 'crash';
+                    result.tests[name] = 'crash';
                 });
                 testsForResultTable(/Web process to crash/).forEach(function(name) {
-                    tests[name] = 'webprocess crash';
+                    result.tests[name] = 'webprocess crash';
                 });
 
-                PersistentCache.set(cacheKey, tests);
-                callback(tests, tooManyFailures);
+                PersistentCache.set(cacheKey, result);
+                callback(result.tests, result.tooManyFailures);
             },
             function(xhr) {
                 // We failed to fetch results.html. run-webkit-tests must have aborted early.
-                PersistentCache.set(cacheKey, tests);
-                errorCallback(tests, tooManyFailures);
+                PersistentCache.set(cacheKey, result);
+                errorCallback(result.tests, result.tooManyFailures);
             });
         }
 
@@ -96,15 +100,15 @@ LayoutTestResultsLoader.prototype = {
         self._builder.getNumberOfFailingTests(parsedBuildName.buildNumber, function(failingTestCount, tooManyFailures) {
             if (failingTestCount < 0) {
                 // The number of failing tests couldn't be determined.
-                PersistentCache.set(cacheKey, tests);
-                errorCallback(tests, tooManyFailures);
+                PersistentCache.set(cacheKey, result);
+                errorCallback(result.tests, result.tooManyFailures);
                 return;
             }
 
             if (!failingTestCount) {
                 // All tests passed.
-                PersistentCache.set(cacheKey, tests);
-                callback(tests, tooManyFailures);
+                PersistentCache.set(cacheKey, result);
+                errorCallback(result.tests, result.tooManyFailures);
                 return;
             }
 

commit 05926a5821aa316493fe5b6d4ca867259be51d56

Make TestFailures never treat a too-many-failures build as a last-passing build for a test
Fixes <http://webkit.org/b/63758> TestFailures sometimes incorrectly says that a build was
the last passing build for a test, when in fact the test wasn't run due to too many failures
causing old-run-webkit-tests to exit early

Reviewed by Anders Carlsson.

* BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/LayoutTestHistoryAnalyzer.js:
(LayoutTestHistoryAnalyzer.prototype._incorporateBuildHistory): If there were too many
failures in this build, assume that all the tests failed in the last build would have failed
in this build, too, had they been run. Introduced some new local variables to reduce typing,
and used them throughout.

* BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/LayoutTestResultsLoader.js:
(LayoutTestResultsLoader.prototype.start): Fix a regression from r89841 where we wouldn't
correctly remember whether old-run-webkit-tests exited early due to too many failures for
builds older than r89610. Also removed an unused parameter while I was at it.

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@90172 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/LayoutTestHistoryAnalyzer.js
@@ -117,10 +117,15 @@ LayoutTestHistoryAnalyzer.prototype = {
         self._loader.start(nextBuildName, function(tests, tooManyFailures) {
             ++self._testRunsSinceLastInterestingChange;
 
-            self._history[nextBuildName] = {
+            var historyItem = {
                 tooManyFailures: tooManyFailures,
                 tests: {},
             };
+            self._history[nextBuildName] = historyItem;
+
+            var previousHistoryItem;
+            if (previousBuildName)
+                previousHistoryItem = self._history[previousBuildName];
 
             var newFlakyTests = self._flakinessDetector.incorporateTestResults(nextBuildName, tests, tooManyFailures);
             if (newFlakyTests.length) {
@@ -134,12 +139,22 @@ LayoutTestHistoryAnalyzer.prototype = {
             }
 
             for (var testName in tests) {
-                if (previousBuildName) {
-                    if (!(testName in self._history[previousBuildName].tests))
+                if (previousHistoryItem) {
+                    if (!(testName in previousHistoryItem.tests))
                         continue;
-                    delete self._history[previousBuildName].tests[testName];
+                    delete previousHistoryItem.tests[testName];
                 }
-                self._history[nextBuildName].tests[testName] = tests[testName];
+                historyItem.tests[testName] = tests[testName];
+            }
+
+            if (tooManyFailures && previousHistoryItem) {
+                // Not all tests were run due to too many failures. Just assume that all the tests
+                // that failed in the last build would still have failed in this build had they been
+                // run.
+                for (var testName in previousHistoryItem.tests) {
+                    historyItem.tests[testName] = previousHistoryItem.tests[testName];
+                    delete previousHistoryItem.tests[testName];
+                }
             }
 
             var previousUnexplainedFailuresCount = previousBuildName ? Object.keys(self._history[previousBuildName].tests).length : 0;

Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/LayoutTestResultsLoader.js
@@ -48,7 +48,7 @@ LayoutTestResultsLoader.prototype = {
 
         var self = this;
 
-        function fetchAndParseResultsHTMLAndCallCallback(callback, tooManyFailures) {
+        function fetchAndParseResultsHTMLAndCallCallback(callback) {
             getResource(self._builder.resultsPageURL(buildName), function(xhr) {
                 var root = document.createElement('html');
                 root.innerHTML = xhr.responseText;
@@ -115,6 +115,8 @@ LayoutTestResultsLoader.prototype = {
         }
 
         self._builder.getNumberOfFailingTests(parsedBuildName.buildNumber, function(failingTestCount, tooManyFailures) {
+            result.tooManyFailures = tooManyFailures;
+
             if (failingTestCount < 0) {
                 // The number of failing tests couldn't be determined.
                 PersistentCache.set(cacheKey, result);