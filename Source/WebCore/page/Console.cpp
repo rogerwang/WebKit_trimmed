rogerwang/WebKit_trimmed

commit 1ecf27821b393bfa539a676e54ea3d1c631b60b2

Web Inspector: Rename console.markTimeline() to console.timeStamp() for Firebug compatibility. https://bugs.webkit.org/show_bug.cgi?id=63317
Patch by Mike West <mkwst@chromium.org> on 2011-07-15
Reviewed by Pavel Feldman.

Source/WebCore:

Test: inspector/timeline/timeline-time-stamp.html

* inspector/InspectorConsoleInstrumentation.h:
(WebCore::InspectorInstrumentation::consoleTimeStamp):
* inspector/InspectorInstrumentation.cpp:
(WebCore::InspectorInstrumentation::consoleTimeStampImpl):
* inspector/InspectorInstrumentation.h:
* inspector/InspectorTimelineAgent.cpp:
(WebCore::InspectorTimelineAgent::didTimeStamp):
* inspector/InspectorTimelineAgent.h:
* inspector/TimelineRecordFactory.cpp:
(WebCore::TimelineRecordFactory::createTimeStampData):
* inspector/TimelineRecordFactory.h:
* inspector/front-end/TimelineAgent.js:
* inspector/front-end/TimelineOverviewPane.js:
(WebInspector.TimelineOverviewPane.prototype.update.markPercentagesForRecord):
(WebInspector.TimelineOverviewPane.prototype.update):
* inspector/front-end/TimelinePanel.js:
(WebInspector.TimelinePanel):
(WebInspector.TimelinePanel.prototype.get _recordStyles):
(WebInspector.TimelinePanel.prototype._updateEventDividers):
(WebInspector.TimelinePanel.prototype._createEventDivider):
(WebInspector.TimelinePanel.prototype._innerAddRecordToTimeline):
(WebInspector.TimelinePanel.prototype._clearPanel):
(WebInspector.TimelinePanel.FormattedRecord.prototype._getRecordDetails):
* page/Console.cpp:
(WebCore::Console::markTimeline):
(WebCore::Console::timeStamp):
* page/Console.h:
* page/Console.idl:

LayoutTests:

* inspector/timeline/resources/timeline-iframe-data.html:
* inspector/timeline/timeline-enum-stability-expected.txt:
* inspector/timeline/timeline-event-dispatch.html:
* inspector/timeline/timeline-mark-timeline-expected.txt:
* inspector/timeline/timeline-mark-timeline.html:
* inspector/timeline/timeline-script-tag-1-expected.txt:
* inspector/timeline/timeline-test.js:

Source/WebCore/page/Console.cpp
@@ -253,9 +253,9 @@ void Console::count(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCall
     InspectorInstrumentation::consoleCount(page(), arguments, callStack);
 }
 
-void Console::markTimeline(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCallStack>)
+void Console::markTimeline(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCallStack> callStack)
 {
-    InspectorInstrumentation::consoleMarkTimeline(page(), arguments);
+    InspectorInstrumentation::consoleTimeStamp(page(), arguments);
 }
 
 #if ENABLE(JAVASCRIPT_DEBUGGER)
@@ -307,6 +307,11 @@ void Console::time(const String& title)
 void Console::timeEnd(const String& title, PassRefPtr<ScriptArguments>, PassRefPtr<ScriptCallStack> callStack)
 {
     InspectorInstrumentation::stopConsoleTiming(page(), title, callStack);
+}
+
+void Console::timeStamp(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCallStack>)
+{
+    InspectorInstrumentation::consoleTimeStamp(page(), arguments);
 }
 
 void Console::group(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCallStack> callStack)

LayoutTests/inspector/timeline/resources/timeline-iframe-data.html
@@ -1,7 +1,7 @@
 <html>
 <head>
 <script>
-console.markTimeline("SCRIPT TAG");
+console.timeStamp("SCRIPT TAG");
 
 function iframeOnload()
 {

LayoutTests/inspector/timeline/timeline-enum-stability-expected.txt
@@ -14,7 +14,7 @@ Applications outside of WebKit depend on the stability of the mapping of these t
     XHRReadyStateChange : "XHRReadyStateChange"
     XHRLoad : "XHRLoad"
     EvaluateScript : "EvaluateScript"
-    MarkTimeline : "MarkTimeline"
+    TimeStamp : "TimeStamp"
     MarkLoad : "MarkLoad"
     MarkDOMContent : "MarkDOMContent"
     ScheduleResourceRequest : "ScheduleResourceRequest"

LayoutTests/inspector/timeline/timeline-event-dispatch.html
@@ -6,7 +6,7 @@
 
 function handleMouseDown(event) 
 {
-    console.markTimeline("Handling mousedown");
+    console.timeStamp("Handling mousedown");
 }
 
 function performActions()

LayoutTests/inspector/timeline/timeline-script-tag-1-expected.txt
@@ -5,7 +5,7 @@ Tests the Timeline API instrumentation of an HTML script tag.
 ParseHTML
 ParseHTML
 ----> EvaluateScript
---------> MarkTimeline : SCRIPT TAG
+--------> TimeStamp : SCRIPT TAG
 EvaluateScript Properties:
 {
     startTime : <number>

LayoutTests/inspector/timeline/timeline-test.js
@@ -83,7 +83,7 @@ InspectorTest.dumpTimelineRecord = function(record, level)
         prefix = "----" + prefix;
     if (level > 0)
         prefix = prefix + "> ";
-    if (record.type === WebInspector.TimelineAgent.RecordType.MarkTimeline) {
+    if (record.type === WebInspector.TimelineAgent.RecordType.TimeStamp) {
         suffix = " : " + record.data.message;
     }
     InspectorTest.addResult(prefix + InspectorTest._timelineAgentTypeToString(record.type) + suffix);

LayoutTests/inspector/timeline/timeline-time-stamp-expected.txt
@@ -1,10 +1,10 @@
-Tests the Timeline API mark feature
+Tests the Timeline API timeStamp feature
 
 TimeStamp Properties:
 {
     startTime : <number>
     data : {
-        message : "MARK TIMELINE"
+        message : "TIME STAMP"
     }
     type : "TimeStamp"
     usedHeapSize : <number>

LayoutTests/inspector/timeline/timeline-time-stamp.html
@@ -6,7 +6,7 @@
 
 function performActions()
 {
-    console.markTimeline("MARK TIMELINE");
+    console.timeStamp("TIME STAMP");
 }
 
 function test()
@@ -19,7 +19,7 @@ function test()
 
 <body onload="runTest()">
 <p>
-Tests the Timeline API mark feature
+Tests the Timeline API timeStamp feature
 </p>
 
 </body>

commit 2b1408e2e82ec78b373860da274d6e4fea4a7998

2011-07-15  Nikolas Zimmermann  <nzimmermann@rim.com>
        Not reviewed. Fix build failure introduced in r91061.

        * page/Console.cpp:
        (WebCore::Console::markTimeline): Remove argument name from unused parameter.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@91065 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/page/Console.cpp
@@ -253,7 +253,7 @@ void Console::count(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCall
     InspectorInstrumentation::consoleCount(page(), arguments, callStack);
 }
 
-void Console::markTimeline(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCallStack> callStack)
+void Console::markTimeline(PassRefPtr<ScriptArguments> arguments, PassRefPtr<ScriptCallStack>)
 {
     InspectorInstrumentation::consoleTimeStamp(page(), arguments);
 }