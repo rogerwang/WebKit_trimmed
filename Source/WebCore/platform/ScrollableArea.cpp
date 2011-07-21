rogerwang/WebKit_trimmed

commit b9a6286246630c30e9b754804aa37f10eab21c06

Scrollbar color heuristic needs to be hooked up in WebKit1 https://bugs.webkit.org/show_bug.cgi?id=64220 <rdar://problem/9589140>
Patch by Tim Horton <timothy_horton@apple.com> on 2011-07-20
Reviewed by Darin Adler.

Store the scroller style on ScrollableArea, and recompute it whenever
the document or body element background color changes or the base background
color of the scrollview changes. Also, make sure to repaint the scrollbars
whenever the style changes.

Source/WebCore:

No new tests, since this code is only enabled on future versions of Mac OS X.

* WebCore.exp.in:
* page/Frame.cpp:
(WebCore::Frame::getDocumentBackgroundColor):
* page/Frame.h:
* page/FrameView.cpp:
(WebCore::FrameView::recalculateScrollbarOverlayStyle):
(WebCore::FrameView::setBaseBackgroundColor):
(WebCore::FrameView::documentBackgroundColor): Renamed.
* page/FrameView.h:
* platform/ScrollView.cpp:
(WebCore::ScrollView::setScrollbarOverlayStyle):
* platform/ScrollView.h:
* platform/ScrollableArea.cpp:
(WebCore::ScrollableArea::setScrollbarOverlayStyle):
* platform/ScrollableArea.h:
(WebCore::ScrollableArea::scrollbarOverlayStyle):
* platform/ScrollbarTheme.h:
(WebCore::ScrollbarTheme::updateScrollbarOverlayStyle):
* platform/mac/ScrollViewMac.mm:
(WebCore::toNSScrollerKnobStyle):
(WebCore::ScrollView::platformSetScrollbarOverlayStyle):
* platform/mac/ScrollbarThemeMac.h:
* platform/mac/ScrollbarThemeMac.mm:
(WebCore::toScrollbarPainterKnobStyle):
(WebCore::ScrollbarThemeMac::updateScrollbarOverlayStyle):
(WebCore::ScrollbarThemeMac::paint):
* rendering/RenderBox.cpp:
(WebCore::RenderBox::styleDidChange):

Source/WebKit2:

* WebProcess/WebPage/WebFrame.cpp:
(WebKit::WebFrame::getDocumentBackgroundColor):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@91435 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/ScrollableArea.cpp
@@ -182,6 +182,9 @@ void ScrollableArea::willEndLiveResize()
 void ScrollableArea::didAddVerticalScrollbar(Scrollbar* scrollbar)
 {
     scrollAnimator()->didAddVerticalScrollbar(scrollbar);
+
+    // <rdar://problem/9797253> AppKit resets the scrollbar's style when you attach a scrollbar
+    setScrollbarOverlayStyle(scrollbarOverlayStyle());
 }
 
 void ScrollableArea::willRemoveVerticalScrollbar(Scrollbar* scrollbar)
@@ -192,6 +195,9 @@ void ScrollableArea::willRemoveVerticalScrollbar(Scrollbar* scrollbar)
 void ScrollableArea::didAddHorizontalScrollbar(Scrollbar* scrollbar)
 {
     scrollAnimator()->didAddHorizontalScrollbar(scrollbar);
+
+    // <rdar://problem/9797253> AppKit resets the scrollbar's style when you attach a scrollbar
+    setScrollbarOverlayStyle(scrollbarOverlayStyle());
 }
 
 void ScrollableArea::willRemoveHorizontalScrollbar(Scrollbar* scrollbar)
@@ -203,6 +209,21 @@ bool ScrollableArea::hasOverlayScrollbars() const
 {
     return (verticalScrollbar() && verticalScrollbar()->isOverlayScrollbar())
         || (horizontalScrollbar() && horizontalScrollbar()->isOverlayScrollbar());
+}
+
+void ScrollableArea::setScrollbarOverlayStyle(ScrollbarOverlayStyle overlayStyle)
+{
+    m_scrollbarOverlayStyle = overlayStyle;
+
+    if (horizontalScrollbar()) {
+        ScrollbarTheme::nativeTheme()->updateScrollbarOverlayStyle(horizontalScrollbar());
+        horizontalScrollbar()->invalidate();
+    }
+    
+    if (verticalScrollbar()) {
+        ScrollbarTheme::nativeTheme()->updateScrollbarOverlayStyle(verticalScrollbar());
+        verticalScrollbar()->invalidate();
+    }
 }
 
 bool ScrollableArea::isPinnedInDirection(const IntSize& scrollDelta) const

Source/WebCore/WebCore.exp.in
@@ -1235,7 +1235,6 @@ __ZNK7WebCore5Frame14selectionImageEb
 __ZNK7WebCore5Frame15contentRendererEv
 __ZNK7WebCore5Frame15layerTreeAsTextEb
 __ZNK7WebCore5Frame18documentTypeStringEv
-__ZNK7WebCore5Frame26getDocumentBackgroundColorEv
 __ZNK7WebCore5Frame31displayStringModifiedByEncodingERKN3WTF6StringE
 __ZNK7WebCore5Frame8settingsEv
 __ZNK7WebCore5Frame9domWindowEv
@@ -1326,6 +1325,7 @@ __ZNK7WebCore9FrameView13paintBehaviorEv
 __ZNK7WebCore9FrameView19baseBackgroundColorEv
 __ZNK7WebCore9FrameView20isSoftwareRenderableEv
 __ZNK7WebCore9FrameView22windowClipRectForLayerEPKNS_11RenderLayerEb
+__ZNK7WebCore9FrameView23documentBackgroundColorEv
 __ZNK7WebCore9FrameView28isEnclosedInCompositingLayerEv
 __ZNK7WebCore9PageCache10frameCountEv
 __ZNK7WebCore9PageCache21autoreleasedPageCountEv

Source/WebCore/page/Frame.cpp
@@ -507,46 +507,6 @@ String Frame::matchLabelsAgainstElement(const Vector<String>& labels, Element* e
     
     return matchLabelsAgainstString(labels, element->getAttribute(idAttr));
 }
-    
-Color Frame::getDocumentBackgroundColor() const
-{
-    // <https://bugs.webkit.org/show_bug.cgi?id=59540> We blend the background color of
-    // the document and the body against the base background color of the frame view.
-    // Background images are unfortunately impractical to include.
-
-    // Return invalid Color objects whenever there is insufficient information.
-    if (!m_doc)
-        return Color();
-
-    Element* htmlElement = m_doc->documentElement();
-    Element* bodyElement = m_doc->body();
-
-    // start as invalid colors
-    Color htmlBackgroundColor;
-    Color bodyBackgroundColor;
-    if (htmlElement && htmlElement->renderer())
-        htmlBackgroundColor = htmlElement->renderer()->style()->visitedDependentColor(CSSPropertyBackgroundColor);
-    if (bodyElement && bodyElement->renderer())
-        bodyBackgroundColor = bodyElement->renderer()->style()->visitedDependentColor(CSSPropertyBackgroundColor);
-    
-    if (!bodyBackgroundColor.isValid()) {
-        if (!htmlBackgroundColor.isValid())
-            return Color();
-        return view()->baseBackgroundColor().blend(htmlBackgroundColor);
-    }
-    
-    if (!htmlBackgroundColor.isValid())
-        return view()->baseBackgroundColor().blend(bodyBackgroundColor);
-    
-    // We take the aggregate of the base background color
-    // the <html> background color, and the <body>
-    // background color to find the document color. The
-    // addition of the base background color is not
-    // technically part of the document background, but it
-    // otherwise poses problems when the aggregate is not
-    // fully opaque.
-    return view()->baseBackgroundColor().blend(htmlBackgroundColor).blend(bodyBackgroundColor);
-}
 
 void Frame::setPrinting(bool printing, const FloatSize& pageSize, float maximumShrinkRatio, AdjustViewSizeOrNot shouldAdjustViewSize)
 {

Source/WebCore/page/Frame.h
@@ -192,8 +192,6 @@ namespace WebCore {
         String searchForLabelsAboveCell(RegularExpression*, HTMLTableCellElement*, size_t* resultDistanceFromStartOfCell);
         String searchForLabelsBeforeElement(const Vector<String>& labels, Element*, size_t* resultDistance, bool* resultIsInCellAbove);
         String matchLabelsAgainstElement(const Vector<String>& labels, Element*);
-
-        Color getDocumentBackgroundColor() const;
         
 #if PLATFORM(MAC)
         NSString* searchForLabelsBeforeElement(NSArray* labels, Element*, size_t* resultDistance, bool* resultIsInCellAbove);

Source/WebCore/page/FrameView.cpp
@@ -325,21 +325,24 @@ void FrameView::willRemoveHorizontalScrollbar(Scrollbar* scrollbar)
         m_frame->document()->didRemoveWheelEventHandler();
 }
 
-ScrollbarOverlayStyle FrameView::recommendedScrollbarOverlayStyle() const
+void FrameView::recalculateScrollbarOverlayStyle()
 {
-    Color bgColor = m_frame->getDocumentBackgroundColor();
-    if (!bgColor.isValid())
-        return ScrollbarOverlayStyleDefault;
-    
-    // Reduce the background color from RGB to a lightness value
-    // and determine which scrollbar style to use based on a lightness
-    // heuristic.
-    double hue, saturation, lightness;
-    bgColor.getHSL(hue, saturation, lightness);
-    if (lightness > .5)
-        return ScrollbarOverlayStyleDefault;
-    
-    return ScrollbarOverlayStyleLight;
+    ScrollbarOverlayStyle oldOverlayStyle = scrollbarOverlayStyle();
+    ScrollbarOverlayStyle overlayStyle = ScrollbarOverlayStyleDefault;
+
+    Color backgroundColor = documentBackgroundColor();
+    if (backgroundColor.isValid()) {
+        // Reduce the background color from RGB to a lightness value
+        // and determine which scrollbar style to use based on a lightness
+        // heuristic.
+        double hue, saturation, lightness;
+        backgroundColor.getHSL(hue, saturation, lightness);
+        if (lightness <= .5)
+            overlayStyle = ScrollbarOverlayStyleLight;
+    }
+
+    if (oldOverlayStyle != overlayStyle)
+        setScrollbarOverlayStyle(overlayStyle);
 }
 
 void FrameView::clear()
@@ -1895,6 +1898,8 @@ void FrameView::setBaseBackgroundColor(const Color& backgroundColor)
         m_baseBackgroundColor = Color::white;
     else
         m_baseBackgroundColor = backgroundColor;
+
+    recalculateScrollbarOverlayStyle();
 }
 
 void FrameView::updateBackgroundRecursively(const Color& backgroundColor, bool transparent)
@@ -2365,6 +2370,46 @@ void FrameView::paintScrollCorner(GraphicsContext* context, const IntRect& corne
     }
 
     ScrollView::paintScrollCorner(context, cornerRect);
+}
+
+Color FrameView::documentBackgroundColor() const
+{
+    // <https://bugs.webkit.org/show_bug.cgi?id=59540> We blend the background color of
+    // the document and the body against the base background color of the frame view.
+    // Background images are unfortunately impractical to include.
+
+    // Return invalid Color objects whenever there is insufficient information.
+    if (!frame()->document())
+        return Color();
+
+    Element* htmlElement = frame()->document()->documentElement();
+    Element* bodyElement = frame()->document()->body();
+
+    // Start with invalid colors.
+    Color htmlBackgroundColor;
+    Color bodyBackgroundColor;
+    if (htmlElement && htmlElement->renderer())
+        htmlBackgroundColor = htmlElement->renderer()->style()->visitedDependentColor(CSSPropertyBackgroundColor);
+    if (bodyElement && bodyElement->renderer())
+        bodyBackgroundColor = bodyElement->renderer()->style()->visitedDependentColor(CSSPropertyBackgroundColor);
+
+    if (!bodyBackgroundColor.isValid()) {
+        if (!htmlBackgroundColor.isValid())
+            return Color();
+        return baseBackgroundColor().blend(htmlBackgroundColor);
+    }
+
+    if (!htmlBackgroundColor.isValid())
+        return baseBackgroundColor().blend(bodyBackgroundColor);
+
+    // We take the aggregate of the base background color
+    // the <html> background color, and the <body>
+    // background color to find the document color. The
+    // addition of the base background color is not
+    // technically part of the document background, but it
+    // otherwise poses problems when the aggregate is not
+    // fully opaque.
+    return baseBackgroundColor().blend(htmlBackgroundColor).blend(bodyBackgroundColor);
 }
 
 bool FrameView::hasCustomScrollbars() const

Source/WebCore/page/FrameView.h
@@ -140,7 +140,7 @@ public:
     void resetScrollbars();
     void resetScrollbarsAndClearContentsSize();
     void detachCustomScrollbars();
-    virtual ScrollbarOverlayStyle recommendedScrollbarOverlayStyle() const;
+    virtual void recalculateScrollbarOverlayStyle();
 
     void clear();
 
@@ -221,6 +221,8 @@ public:
 
     virtual void paintOverhangAreas(GraphicsContext*, const IntRect& horizontalOverhangArea, const IntRect& verticalOverhangArea, const IntRect& dirtyRect);
     virtual void paintScrollCorner(GraphicsContext*, const IntRect& cornerRect);
+
+    Color documentBackgroundColor() const;
 
     static double currentPaintTimeStamp() { return sCurrentPaintTimeStamp; } // returns 0 if not painting
     

Source/WebCore/platform/ScrollView.cpp
@@ -779,6 +779,12 @@ Scrollbar* ScrollView::scrollbarAtPoint(const IntPoint& windowPoint)
     return 0;
 }
 
+void ScrollView::setScrollbarOverlayStyle(ScrollbarOverlayStyle overlayStyle)
+{
+    ScrollableArea::setScrollbarOverlayStyle(overlayStyle);
+    platformSetScrollbarOverlayStyle(overlayStyle);
+}
+
 void ScrollView::wheelEvent(PlatformWheelEvent& e)
 {
     // We don't allow mouse wheeling to happen in a ScrollView that has had its scrollbars explicitly disabled.
@@ -1276,6 +1282,10 @@ void ScrollView::platformSetScrollbarsSuppressed(bool)
 }
 
 void ScrollView::platformSetScrollOrigin(const IntPoint&, bool updatePositionAtAll, bool updatePositionSynchronously)
+{
+}
+
+void ScrollView::platformSetScrollbarOverlayStyle(ScrollbarOverlayStyle)
 {
 }
 

Source/WebCore/platform/ScrollView.h
@@ -108,6 +108,8 @@ public:
 
     virtual bool avoidScrollbarCreation() const { return false; }
 
+    virtual void setScrollbarOverlayStyle(ScrollbarOverlayStyle);
+
     // By default you only receive paint events for the area that is visible. In the case of using a
     // tiled backing store, this function can be set, so that the view paints the entire contents.
     bool paintsEntireContents() const { return m_paintsEntireContents; }
@@ -389,6 +391,7 @@ private:
     void platformSetScrollbarsSuppressed(bool repaintOnUnsuppress);
     void platformRepaintContentRectangle(const IntRect&, bool now);
     bool platformIsOffscreen() const;
+    void platformSetScrollbarOverlayStyle(ScrollbarOverlayStyle);
    
     void platformSetScrollOrigin(const IntPoint&, bool updatePositionAtAll, bool updatePositionSynchronously);
 

commit 0455e692328b96f6c69fe19db2cd7a48fcbfd678

Fix a Valgrind failure caused by r91435.
* platform/ScrollableArea.cpp:
(WebCore::ScrollableArea::ScrollableArea):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@91447 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/ScrollableArea.cpp
@@ -47,6 +47,7 @@ ScrollableArea::ScrollableArea()
     , m_inLiveResize(false)
     , m_verticalScrollElasticity(ScrollElasticityNone)
     , m_horizontalScrollElasticity(ScrollElasticityNone)
+    , m_scrollbarOverlayStyle(ScrollbarOverlayStyleDefault)
 {
 }
 