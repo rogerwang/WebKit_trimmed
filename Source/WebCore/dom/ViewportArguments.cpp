rogerwang/WebKit_trimmed

commit d68f698d8ddf3922a6d6c9b88e84243a68cb2fd0

Differentiate implicit viewport from that of the meta tag https://bugs.webkit.org/show_bug.cgi?id=71453
Reviewed by Simon Hausmann.

This is needed because of DPI adjustment taking place with the meta
tag. This is to be avoided when no viewport meta tag is present.

* dom/Document.cpp:
(WebCore::Document::processViewport):
* dom/ViewportArguments.cpp:
(WebCore::computeViewportAttributes):
* dom/ViewportArguments.h:
(WebCore::ViewportArguments::ViewportArguments):
(WebCore::ViewportArguments::operator==):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@99173 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/ViewportArguments.cpp
@@ -50,6 +50,17 @@ ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktop
 
     ASSERT(availableWidth > 0 && availableHeight > 0);
 
+    float autoDPI;
+
+    switch (args.type) {
+    case ViewportArguments::Implicit:
+        autoDPI = deviceDPI;
+        break;
+    case ViewportArguments::ViewportMeta:
+        autoDPI = 160;
+        break;
+    }
+
     switch (int(args.targetDensityDpi)) {
     case ViewportArguments::ValueDeviceDPI:
         args.targetDensityDpi = deviceDPI;
@@ -58,6 +69,8 @@ ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktop
         args.targetDensityDpi = 120;
         break;
     case ViewportArguments::ValueAuto:
+        args.targetDensityDpi = autoDPI;
+        break;
     case ViewportArguments::ValueMediumDPI:
         args.targetDensityDpi = 160;
         break;

Source/WebCore/dom/Document.cpp
@@ -2670,7 +2670,7 @@ void Document::processViewport(const String& features)
 {
     ASSERT(!features.isNull());
 
-    m_viewportArguments = ViewportArguments();
+    m_viewportArguments = ViewportArguments(ViewportArguments::ViewportMeta);
     processArguments(features, (void*)&m_viewportArguments, &setViewportFeature);
 
     Frame* frame = this->frame();

Source/WebCore/dom/ViewportArguments.h
@@ -56,6 +56,11 @@ struct ViewportAttributes {
 
 struct ViewportArguments {
 
+    enum Type {
+        Implicit,
+        ViewportMeta
+    } type;
+
     enum {
         ValueAuto = -1,
         ValueDesktopWidth = -2,
@@ -67,8 +72,9 @@ struct ViewportArguments {
         ValueHighDPI = -8
     };
 
-    ViewportArguments()
-        : initialScale(ValueAuto)
+    ViewportArguments(Type type = Implicit)
+        : type(type)
+        , initialScale(ValueAuto)
         , minimumScale(ValueAuto)
         , maximumScale(ValueAuto)
         , width(ValueAuto)
@@ -88,6 +94,8 @@ struct ViewportArguments {
 
     bool operator==(const ViewportArguments& other) const
     {
+        // Used for figuring out whether to reset the viewport or not,
+        // thus we are not taking type into account.
         return initialScale == other.initialScale
             && minimumScale == other.minimumScale
             && maximumScale == other.maximumScale

commit 0923e72001ee27732bd4253aa45318e709d51aae

2011-11-03  Pavel Feldman  <pfeldman@google.com>
        Not reviewed: win build fix. Assign autoDPI at all cases (breakage introduced in r99173).

        * dom/ViewportArguments.cpp:
        (WebCore::computeViewportAttributes):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@99175 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/dom/ViewportArguments.cpp
@@ -50,7 +50,7 @@ ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktop
 
     ASSERT(availableWidth > 0 && availableHeight > 0);
 
-    float autoDPI;
+    float autoDPI = deviceDPI;
 
     switch (args.type) {
     case ViewportArguments::Implicit: