rogerwang/WebKit_trimmed

commit e7fc18ac1bbae7f865bed90b599eb56015bc69a8

remove CSSBorderImageValue https://bugs.webkit.org/show_bug.cgi?id=75563
Reviewed by Tony Chang.

Source/WebCore:

Remove CSSBorderImageValue as border-image is a shorthand therefore we don't
need a dedicated CSS class type for it. CSSBorderImageValue was here for
-webkit-border-image which is not a shorthand. This is the first step to move border-image
close to a correct shorthand implementation while keeping -webkit-border-image being a regular
CSS property.

No new tests : It's a refactor, existing tests should cover it.

* CMakeLists.txt:
* GNUmakefile.list.am:
* Target.pri:
* WebCore.gypi:
* WebCore.order:
* WebCore.vcproj/WebCore.vcproj:
* WebCore.xcodeproj/project.pbxproj:
* css/CSSAllInOne.cpp:
* css/CSSBorderImage.cpp: Added.
(WebCore::createBorderImageValue):
* css/CSSBorderImage.h: Added.
* css/CSSBorderImageValue.cpp: Removed.
* css/CSSBorderImageValue.h: Removed.
* css/CSSComputedStyleDeclaration.cpp:
(WebCore::valueForNinePieceImage):
* css/CSSParser.cpp:
(WebCore::BorderImageParseContext::commitBorderImage):
* css/CSSStyleSelector.cpp:
(WebCore::CSSStyleSelector::mapNinePieceImage):
* css/CSSValue.cpp:
(WebCore::CSSValue::addSubresourceStyleURLs):
(WebCore::CSSValue::cssText):
(WebCore::CSSValue::destroy):
* css/CSSValue.h:

LayoutTests:

Modify the expected output as now border-image is a CSSValueList.

* fast/css/getComputedStyle/getComputedStyle-border-image-expected.txt:


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@105502 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/css/CSSBorderImage.cpp
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include "CSSBorderImage.h"
+
+namespace WebCore {
+
+PassRefPtr<CSSValueList> createBorderImageValue(PassRefPtr<CSSValue> image, PassRefPtr<CSSValue> imageSlice, PassRefPtr<CSSValue> borderSlice,
+                                                PassRefPtr<CSSValue> outset, PassRefPtr<CSSValue> repeat)
+{
+    RefPtr<CSSValueList> list = CSSValueList::createSpaceSeparated();
+    list->append(image);
+
+    if (borderSlice || outset) {
+        RefPtr<CSSValueList> listSlash = CSSValueList::createSlashSeparated();
+        if (imageSlice)
+            listSlash->append(imageSlice);
+
+        if (borderSlice)
+            listSlash->append(borderSlice);
+
+        if (outset)
+            listSlash->append(outset);
+
+        list->append(listSlash);
+    } else if (imageSlice)
+        list->append(imageSlice);
+    if (repeat)
+        list->append(repeat);
+    return list.release();
+}
+
+} // namespace WebCore

Source/WebCore/css/CSSStyleSelector.cpp
@@ -33,7 +33,7 @@
 #include "ContentData.h"
 #include "Counter.h"
 #include "CounterContent.h"
-#include "CSSBorderImageValue.h"
+#include "CSSBorderImage.h"
 #include "CSSCursorImageValue.h"
 #include "CSSFontFaceRule.h"
 #include "CSSFontSelector.h"
@@ -4319,11 +4319,11 @@ void CSSStyleSelector::mapAnimationTimingFunction(Animation* animation, CSSValue
 void CSSStyleSelector::mapNinePieceImage(CSSPropertyID property, CSSValue* value, NinePieceImage& image)
 {
     // If we're a primitive value, then we are "none" and don't need to alter the empty image at all.
-    if (!value || value->isPrimitiveValue() || !value->isBorderImageValue())
+    if (!value || value->isPrimitiveValue())
         return;
 
     // Retrieve the border image value.
-    CSSBorderImageValue* borderImage = static_cast<CSSBorderImageValue*>(value);
+    CSSValueList* borderImage = static_cast<CSSValueList*>(value);
 
     // Set the image (this kicks off the load).
     CSSPropertyID imageProperty;
@@ -4334,19 +4334,40 @@ void CSSStyleSelector::mapNinePieceImage(CSSPropertyID property, CSSValue* value
     else
         imageProperty = property;
 
-    if (CSSValue* imageValue = borderImage->imageValue())
+    if (CSSValue* imageValue = borderImage->item(0))
         image.setImage(styleImage(imageProperty, imageValue));
 
-    // Map in the image slices.
-    mapNinePieceImageSlice(borderImage->m_imageSlice.get(), image);
+    if (borderImage->item(1)) {
+        if (borderImage->item(1)->cssValueType() != CSSValue::CSS_VALUE_LIST) {
+            // Map in the image slices.
+            if (borderImage->item(1)) {
+                if (borderImage->item(1)->isBorderImageSliceValue()) {
+                    mapNinePieceImageSlice(borderImage->item(1), image);
+                     if (borderImage->item(2))
+                        // Set the appropriate rules for stretch/round/repeat of the slices
+                        mapNinePieceImageRepeat(borderImage->item(2), image);
+                } else
+                    // Set the appropriate rules for stretch/round/repeat of the slices
+                    mapNinePieceImageRepeat(borderImage->item(1), image);
+            }
+        } else {
+            CSSValueList* slashList = static_cast<CSSValueList*>(borderImage->item(1));
+            // Map in the image slices.
+            if (slashList->item(0) && slashList->item(0)->isBorderImageSliceValue())
+                mapNinePieceImageSlice(slashList->item(0), image);
 
-    // Map in the border slices.
-    if (borderImage->m_borderSlice)
-        image.setBorderSlices(mapNinePieceImageQuad(borderImage->m_borderSlice.get()));
+            // Map in the border slices.
+            if (slashList->item(1))
+                image.setBorderSlices(mapNinePieceImageQuad(slashList->item(1)));
 
-    // Map in the outset.
-    if (borderImage->m_outset)
-        image.setOutset(mapNinePieceImageQuad(borderImage->m_outset.get()));
+            // Map in the outset.
+            if (slashList->item(2))
+                image.setOutset(mapNinePieceImageQuad(slashList->item(2)));
+
+            // Set the appropriate rules for stretch/round/repeat of the slices
+            mapNinePieceImageRepeat(borderImage->item(2), image);
+        }
+    }
 
     if (property == CSSPropertyWebkitBorderImage) {
         // We have to preserve the legacy behavior of -webkit-border-image and make the border slices
@@ -4361,9 +4382,6 @@ void CSSStyleSelector::mapNinePieceImage(CSSPropertyID property, CSSValue* value
         if (image.borderSlices().left().isFixed())
             style()->setBorderLeftWidth(image.borderSlices().left().value());
     }
-
-    // Set the appropriate rules for stretch/round/repeat of the slices
-    mapNinePieceImageRepeat(borderImage->m_repeat.get(), image);
 }
 
 void CSSStyleSelector::mapNinePieceImageSlice(CSSValue* value, NinePieceImage& image)

LayoutTests/fast/css/getComputedStyle/getComputedStyle-border-image-expected.txt
@@ -8,20 +8,20 @@ Test calling getPropertyValue on computed styles for -webkit-border-image proper
 -webkit-border-image: -webkit-gradient(linear, 0 0, 0 0) 25 25 25 25 stretch stretch;
   -webkit-border-image
     getPropertyValue:    -webkit-gradient(linear, 0 0, 0 0) 25 fill / 1 / 0px stretch
-    getPropertyCSSValue: [object CSSValue]
+    getPropertyCSSValue: [object CSSValueList]
 
 -webkit-border-image: -webkit-gradient(linear, 0 0, 0 0) 50 repeat;
   -webkit-border-image
     getPropertyValue:    -webkit-gradient(linear, 0 0, 0 0) 50 fill / 1 / 0px repeat
-    getPropertyCSSValue: [object CSSValue]
+    getPropertyCSSValue: [object CSSValueList]
 
 -webkit-border-image: -webkit-gradient(linear, 0 0, 0 0) 25 25 25 25 / 20 20 20 20 repeat;
   -webkit-border-image
     getPropertyValue:    -webkit-gradient(linear, 0 0, 0 0) 25 fill / 20 / 0px repeat
-    getPropertyCSSValue: [object CSSValue]
+    getPropertyCSSValue: [object CSSValueList]
 
 -webkit-border-image: -webkit-gradient(linear, 0 0, 0 0) 50 / 20 stretch stretch;
   -webkit-border-image
     getPropertyValue:    -webkit-gradient(linear, 0 0, 0 0) 50 fill / 20 / 0px stretch
-    getPropertyCSSValue: [object CSSValue]
+    getPropertyCSSValue: [object CSSValueList]
 

Source/WebCore/CMakeLists.txt
@@ -426,7 +426,7 @@ SET(WebCore_SOURCES
     bindings/generic/RuntimeEnabledFeatures.cpp
 
     css/CSSAspectRatioValue.cpp
-    css/CSSBorderImageValue.cpp
+    css/CSSBorderImage.cpp
     css/CSSBorderImageSliceValue.cpp
     css/CSSCanvasValue.cpp
     css/CSSCharsetRule.cpp

Source/WebCore/GNUmakefile.list.am
@@ -1272,8 +1272,8 @@ webcore_sources += \
 	Source/WebCore/css/CSSAspectRatioValue.h \
 	Source/WebCore/css/CSSBorderImageSliceValue.cpp \
 	Source/WebCore/css/CSSBorderImageSliceValue.h \
-	Source/WebCore/css/CSSBorderImageValue.cpp \
-	Source/WebCore/css/CSSBorderImageValue.h \
+	Source/WebCore/css/CSSBorderImage.cpp \
+	Source/WebCore/css/CSSBorderImage.h \
 	Source/WebCore/css/CSSCanvasValue.cpp \
 	Source/WebCore/css/CSSCanvasValue.h \
 	Source/WebCore/css/CSSCharsetRule.cpp \

Source/WebCore/Target.pri
@@ -392,7 +392,7 @@ v8 {
 SOURCES += \
     css/CSSAspectRatioValue.cpp \
     css/CSSBorderImageSliceValue.cpp \
-    css/CSSBorderImageValue.cpp \
+    css/CSSBorderImage.cpp \
     css/CSSCanvasValue.cpp \
     css/CSSCharsetRule.cpp \
     css/CSSComputedStyleDeclaration.cpp \
@@ -1504,7 +1504,7 @@ v8 {
 HEADERS += \
     css/CSSAspectRatioValue.h \
     css/CSSBorderImageSliceValue.h \
-    css/CSSBorderImageValue.h \
+    css/CSSBorderImage.h \
     css/CSSCanvasValue.h \
     css/CSSCharsetRule.h \
     css/CSSComputedStyleDeclaration.h \

Source/WebCore/WebCore.gypi
@@ -2369,8 +2369,8 @@
             'config.h',
             'css/CSSAspectRatioValue.cpp',
             'css/CSSAspectRatioValue.h',
-            'css/CSSBorderImageValue.cpp',
-            'css/CSSBorderImageValue.h',
+            'css/CSSBorderImage.cpp',
+            'css/CSSBorderImage.h',
             'css/CSSBorderImageSliceValue.cpp',
             'css/CSSBorderImageSliceValue.h',
             'css/CSSCanvasValue.cpp',

Source/WebCore/WebCore.order
@@ -10459,10 +10459,7 @@ __ZN7WebCore13RenderReplicaD0Ev
 __ZN7WebCore15CSSReflectValueD0Ev
 __ZN7WebCore9CSSParser16parseBorderImageEibRN3WTF6RefPtrINS_8CSSValueEEE
 __ZN7WebCore23BorderImageParseContext17commitBorderImageEPNS_9CSSParserEb
-__ZN7WebCore19CSSBorderImageValue6createEN3WTF10PassRefPtrINS_8CSSValueEEENS2_INS_4RectEEEii
-__ZN7WebCore19CSSBorderImageValueC1EN3WTF10PassRefPtrINS_8CSSValueEEENS2_INS_4RectEEEii
 __ZN7WebCore23BorderImageParseContextD1Ev
-__ZNK7WebCore19CSSBorderImageValue18isBorderImageValueEv
 __ZNK7WebCore22CSSImageGeneratorValue11isFixedSizeEv
 __ZN7WebCore16CSSGradientValue5imageEPNS_12RenderObjectERKNS_7IntSizeE
 __ZNK3WTF9HashTableIPN7WebCore12RenderObjectESt4pairIS3_S4_INS1_7IntSizeEiEENS_18PairFirstExtractorIS7_EENS_7PtrHashIS3_EENS_14PairHashTraitsINS_10HashTraitsIS3_EENSD_IS6_EEEESE_E8containsIS3_NS_22IdentityHashTranslatorIS3_S7_SB_EEEEbRKT_
@@ -10486,7 +10483,6 @@ __ZN7WebCore15GraphicsContext8fillRectERKNS_9FloatRectERNS_9GeneratorE
 __ZN7WebCore8Gradient4fillEPNS_15GraphicsContextERKNS_9FloatRectE
 __ZNK3WTF9HashTableIN7WebCore7IntSizeESt4pairIS2_jENS_18PairFirstExtractorIS4_EENS_7IntHashIS2_EENS_14PairHashTraitsINS_10HashTraitsIS2_EENSA_IjEEEESB_E8containsIS2_NS_22IdentityHashTranslatorIS2_S4_S8_EEEEbRKT_
 __ZN7WebCore14GeneratedImageD0Ev
-__ZN7WebCore19CSSBorderImageValueD0Ev
 __ZN7WebCore22CSSLinearGradientValueD0Ev
 __ZN7WebCore16CSSGradientValueD2Ev
 __ZN3WTF16VectorDestructorILb1EN7WebCore20CSSGradientColorStopEE8destructEPS2_S4_
@@ -19654,7 +19650,6 @@ __ZNK7WebCore24ShorthandPropertyWrapper18isShorthandWrapperEv
 __ZNK7WebCore32PropertyWrapperMaybeInvalidColor5blendEPKNS_13AnimationBaseEPNS_11RenderStyleEPKS4_S7_d
 __ZN7WebCore17CSSParserSelector16insertTagHistoryENS_11CSSSelector8RelationEN3WTF10PassOwnPtrIS0_EES2_
 __ZN7WebCore18SliderThumbElement19defaultEventHandlerEPNS_5EventE
-__ZNK7WebCore19CSSBorderImageValue7cssTextEv
 __ZN7WebCore26jsElementChildElementCountEPN3JSC9ExecStateENS0_7JSValueERKNS0_10IdentifierE
 __ZNK7WebCore7Element17childElementCountEv
 __ZN7WebCore18jsDOMWindowScrollYEPN3JSC9ExecStateENS0_7JSValueERKNS0_10IdentifierE
@@ -31096,7 +31091,6 @@ __ZN7WebCore12CSSStyleRule23addSubresourceStyleURLsERN3WTF11ListHashSetINS_4KURL
 __ZNK7WebCore16HTMLStyleElement27addSubresourceAttributeURLsERN3WTF11ListHashSetINS_4KURLELm256ENS_8KURLHashEEE
 __ZN7WebCore15CSSFontFaceRule23addSubresourceStyleURLsERN3WTF11ListHashSetINS_4KURLELm256ENS_8KURLHashEEE
 __ZN7WebCore19CSSFontFaceSrcValue23addSubresourceStyleURLsERN3WTF11ListHashSetINS_4KURLELm256ENS_8KURLHashEEEPKNS_13CSSStyleSheetE
-__ZN7WebCore19CSSBorderImageValue23addSubresourceStyleURLsERN3WTF11ListHashSetINS_4KURLELm256ENS_8KURLHashEEEPKNS_13CSSStyleSheetE
 __ZN7WebCore15CSSReflectValue23addSubresourceStyleURLsERN3WTF11ListHashSetINS_4KURLELm256ENS_8KURLHashEEEPKNS_13CSSStyleSheetE
 __ZNK7WebCore20HTMLFrameElementBase14isURLAttributeEPNS_9AttributeE
 __ZN7WebCore12IconDatabase25setPrivateBrowsingEnabledEb

commit 1abe7c788465426cfe0b14eacb776d9a5a9a89b8

border-image should not crash when the source is not specified. https://bugs.webkit.org/show_bug.cgi?id=77001
Reviewed by Andreas Kling.

Source/WebCore:

This bug was introduced by r105502 but was exposed by r105738.
The image-source of a border-image is not mandatory therefore it
may happen that you have no value set for it. WebCore::createBorderImageValue
was wrongly assuming that the image is always set. This problem also required a bit
of refactoring in CSSStyleSelector::mapNinePieceImage to take into account that
the image could be optional (just like other properties).

Test: fast/css/border-image-null-image-crash.html

* css/CSSBorderImage.cpp:
(WebCore::createBorderImageValue):
* css/CSSStyleSelector.cpp:
(WebCore::CSSStyleSelector::mapNinePieceImage):

LayoutTests:

Add a new test to cover this crash specifically.

* fast/css/border-image-null-image-crash-expected.txt: Added.
* fast/css/border-image-null-image-crash.html: Added.


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@105898 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/css/CSSBorderImage.cpp
@@ -26,7 +26,8 @@ PassRefPtr<CSSValueList> createBorderImageValue(PassRefPtr<CSSValue> image, Pass
                                                 PassRefPtr<CSSValue> outset, PassRefPtr<CSSValue> repeat)
 {
     RefPtr<CSSValueList> list = CSSValueList::createSpaceSeparated();
-    list->append(image);
+    if (image)
+        list->append(image);
 
     if (borderSlice || outset) {
         RefPtr<CSSValueList> listSlash = CSSValueList::createSlashSeparated();

Source/WebCore/css/CSSStyleSelector.cpp
@@ -4279,8 +4279,8 @@ void CSSStyleSelector::mapAnimationTimingFunction(Animation* animation, CSSValue
 
 void CSSStyleSelector::mapNinePieceImage(CSSPropertyID property, CSSValue* value, NinePieceImage& image)
 {
-    // If we're a primitive value, then we are "none" and don't need to alter the empty image at all.
-    if (!value || value->isPrimitiveValue())
+    // If we're not a value list, then we are "none" and don't need to alter the empty image at all.
+    if (!value || !value->isValueList())
         return;
 
     // Retrieve the border image value.
@@ -4295,24 +4295,15 @@ void CSSStyleSelector::mapNinePieceImage(CSSPropertyID property, CSSValue* value
     else
         imageProperty = property;
 
-    if (CSSValue* imageValue = borderImage->item(0))
-        image.setImage(styleImage(imageProperty, imageValue));
+    for (unsigned i = 0 ; i < borderImage->length() ; ++i) {
+        CSSValue* current = borderImage->item(i);
 
-    if (borderImage->item(1)) {
-        if (borderImage->item(1)->cssValueType() != CSSValue::CSS_VALUE_LIST) {
-            // Map in the image slices.
-            if (borderImage->item(1)) {
-                if (borderImage->item(1)->isBorderImageSliceValue()) {
-                    mapNinePieceImageSlice(borderImage->item(1), image);
-                     if (borderImage->item(2))
-                        // Set the appropriate rules for stretch/round/repeat of the slices
-                        mapNinePieceImageRepeat(borderImage->item(2), image);
-                } else
-                    // Set the appropriate rules for stretch/round/repeat of the slices
-                    mapNinePieceImageRepeat(borderImage->item(1), image);
-            }
-        } else {
-            CSSValueList* slashList = static_cast<CSSValueList*>(borderImage->item(1));
+        if (current->isImageValue() || current->isImageGeneratorValue())
+            image.setImage(styleImage(imageProperty, current));
+        else if (current->isBorderImageSliceValue())
+            mapNinePieceImageSlice(current, image);
+        else if (current->isValueList()) {
+            CSSValueList* slashList = static_cast<CSSValueList*>(current);
             // Map in the image slices.
             if (slashList->item(0) && slashList->item(0)->isBorderImageSliceValue())
                 mapNinePieceImageSlice(slashList->item(0), image);
@@ -4324,9 +4315,9 @@ void CSSStyleSelector::mapNinePieceImage(CSSPropertyID property, CSSValue* value
             // Map in the outset.
             if (slashList->item(2))
                 image.setOutset(mapNinePieceImageQuad(slashList->item(2)));
-
-            // Set the appropriate rules for stretch/round/repeat of the slices
-            mapNinePieceImageRepeat(borderImage->item(2), image);
+        } else if (current->isPrimitiveValue()) {
+            // Set the appropriate rules for stretch/round/repeat of the slices.
+            mapNinePieceImageRepeat(current, image);
         }
     }
 

LayoutTests/fast/css/border-image-null-image-crash-expected.txt
@@ -1,0 +1,10 @@
+Tests that shorthand border-image with a null image doesn't crash.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS computedStyle.getPropertyValue('border-image') is 'none'
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/css/border-image-null-image-crash.html
@@ -1,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<script>
+
+description("Tests that shorthand border-image with a null image doesn't crash.");
+
+var testContainer = document.createElement("div");
+document.body.appendChild(testContainer);
+
+testContainer.innerHTML = '<div id="test">hello</div>';
+
+e = document.getElementById('test');
+computedStyle = window.getComputedStyle(e, null);
+e.style.borderImage = "10% fill";
+
+shouldBe("computedStyle.getPropertyValue('border-image')", "'none'");
+
+document.body.removeChild(testContainer);
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>