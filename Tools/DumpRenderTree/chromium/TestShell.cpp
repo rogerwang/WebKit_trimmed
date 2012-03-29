rogerwang/WebKit_trimmed

commit a6b11c0087c6f44cfa666ce7e3b1df3ecbbb6bfc

[chromium] Fix --enabled-threaded-compositing flag in DRT https://bugs.webkit.org/show_bug.cgi?id=82405
Reviewed by James Robinson.

--enable-threaded-compositing wasn't working because there was no way
to set the value before it was used in the TestShell constructor.

To avoid adding yet another boolean parameter to TestShell's
constructor, add an explicit initialize function that can operate on
any boolean settings that are set post-constructor.

* DumpRenderTree/chromium/DumpRenderTree.cpp:
(main):
* DumpRenderTree/chromium/TestShell.cpp:
(TestShell::TestShell):
(TestShell::initialize):
* DumpRenderTree/chromium/TestShell.h:
(TestShell):
(TestShell::setTestShellMode):


git-svn-id: http://svn.webkit.org/repository/webkit/trunk@112354 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/DumpRenderTree/chromium/TestShell.cpp
@@ -98,11 +98,11 @@ static void makeCanvasOpaque(SkCanvas* canvas)
     }
 }
 
-TestShell::TestShell(bool testShellMode)
+TestShell::TestShell()
     : m_testIsPending(false)
     , m_testIsPreparing(false)
     , m_focusedWidget(0)
-    , m_testShellMode(testShellMode)
+    , m_testShellMode(false)
     , m_devTools(0)
     , m_allowExternalPages(false)
     , m_acceleratedCompositingForVideoEnabled(false)
@@ -131,7 +131,10 @@ TestShell::TestShell(bool testShellMode)
     WebRuntimeFeatures::enableShadowDOM(true);
     WebRuntimeFeatures::enableStyleScoped(true);
     WebRuntimeFeatures::enableScriptedSpeech(true);
+}
 
+void TestShell::initialize()
+{
     m_webPermissions = adoptPtr(new WebPermissions(this));
     m_accessibilityController = adoptPtr(new AccessibilityController(this));
     m_gamepadController = adoptPtr(new GamepadController(this));
@@ -150,7 +153,6 @@ TestShell::TestShell(bool testShellMode)
         WebCompositor::initialize(m_webCompositorThread.get());
     } else
         WebCompositor::initialize(0);
-
 
     // 30 second is the same as the value in Mac DRT.
     // If we use a value smaller than the timeout value of

Tools/DumpRenderTree/chromium/DumpRenderTree.cpp
@@ -234,7 +234,8 @@ int main(int argc, char* argv[])
         openStartupDialog();
 
     { // Explicit scope for the TestShell instance.
-        TestShell shell(testShellMode);
+        TestShell shell;
+        shell.setTestShellMode(testShellMode);
         shell.setAllowExternalPages(allowExternalPages);
         shell.setAcceleratedCompositingForVideoEnabled(acceleratedCompositingForVideoEnabled);
         shell.setThreadedCompositingEnabled(threadedCompositingEnabled);
@@ -251,6 +252,7 @@ int main(int argc, char* argv[])
             // 0x20000000ms is big enough for the purpose to avoid timeout in debugging.
             shell.setLayoutTestTimeout(0x20000000);
         }
+        shell.initialize();
         if (serverMode && !tests.size()) {
 #if OS(ANDROID)
             // Send a signal to host to indicate DRT is ready to process commands.

Tools/DumpRenderTree/chromium/TestShell.h
@@ -82,8 +82,10 @@ struct TestParams {
 
 class TestShell {
 public:
-    TestShell(bool testShellMode);
+    TestShell();
     ~TestShell();
+
+    void initialize();
 
     // The main WebView.
     WebKit::WebView* webView() const { return m_webView; }
@@ -130,6 +132,7 @@ public:
     bool allowExternalPages() const { return m_allowExternalPages; }
     void setAllowExternalPages(bool allowExternalPages) { m_allowExternalPages = allowExternalPages; }
 
+    void setTestShellMode(bool testShellMode) { m_testShellMode = testShellMode; }
     void setAcceleratedCompositingForVideoEnabled(bool enabled) { m_acceleratedCompositingForVideoEnabled = enabled; }
     void setThreadedCompositingEnabled(bool enabled) { m_threadedCompositingEnabled = enabled; }
     void setCompositeToTexture(bool enabled) { m_compositeToTexture = enabled; }

commit 24324bd89be79f87a0916c8b070b627a1fac873a

Web Inspector: chromium: DRT --no-timeout option doesn't work. https://bugs.webkit.org/show_bug.cgi?id=82608
Initial value for m_timeout was initialized in constructor and was overwritten in DRT::main.
This was broken by r112354 and the sequence became opposite.

Reviewed by Yury Semikhatsky.

* DumpRenderTree/chromium/TestShell.cpp:
(TestShell::TestShell):
(TestShell::initialize):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@112552 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Tools/DumpRenderTree/chromium/TestShell.cpp
@@ -131,6 +131,12 @@ TestShell::TestShell()
     WebRuntimeFeatures::enableShadowDOM(true);
     WebRuntimeFeatures::enableStyleScoped(true);
     WebRuntimeFeatures::enableScriptedSpeech(true);
+
+    // 30 second is the same as the value in Mac DRT.
+    // If we use a value smaller than the timeout value of
+    // (new-)run-webkit-tests, (new-)run-webkit-tests misunderstands that a
+    // timed-out DRT process was crashed.
+    m_timeout = 30 * 1000;
 }
 
 void TestShell::initialize()
@@ -153,12 +159,6 @@ void TestShell::initialize()
         WebCompositor::initialize(m_webCompositorThread.get());
     } else
         WebCompositor::initialize(0);
-
-    // 30 second is the same as the value in Mac DRT.
-    // If we use a value smaller than the timeout value of
-    // (new-)run-webkit-tests, (new-)run-webkit-tests misunderstands that a
-    // timed-out DRT process was crashed.
-    m_timeout = 30 * 1000;
 
     createMainWindow();
 }