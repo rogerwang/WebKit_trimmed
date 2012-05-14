rogerwang/WebKit_trimmed

commit 96a6c941e93f543f3607f884e79649dd6c5e9eb4

[TextureMapper] Tiles are not created for large textures https://bugs.webkit.org/show_bug.cgi?id=86245
Patch by Martin Robinson <mrobinson@igalia.com> on 2012-05-11
Reviewed by Noam Rosenthal.

No new tests. This will not produce any observable behavior changes,
unless run on a machine with a small texture size limit.

The maxTextureSize() method on TextureMapperGL was missing a "const"
keyword, meaning that it was not properly overriding the version in
the abstract base class (TextureMapper). This patch adds the const
modifier and cleans up the list of override methods in the two
TextureMapper sublcasses, adding the OVERRIDE macro for compilers that
support it and removing a couple unused methods.

* platform/graphics/texmap/TextureMapperGL.cpp:
* platform/graphics/texmap/TextureMapperGL.h:
(WebCore::TextureMapperGL::create):
* platform/graphics/texmap/TextureMapperImageBuffer.h:
(TextureMapperImageBuffer):

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@116834 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/texmap/TextureMapperGL.h
@@ -37,6 +37,7 @@ class TextureMapperShaderProgram;
 // An OpenGL-ES2 implementation of TextureMapper.
 class TextureMapperGL : public TextureMapper {
 public:
+    static PassOwnPtr<TextureMapperGL> create() { return adoptPtr(new TextureMapperGL); }
     TextureMapperGL();
     virtual ~TextureMapperGL();
 
@@ -47,24 +48,20 @@ public:
 
     typedef int Flags;
 
-    // reimps from TextureMapper
-    virtual void drawBorder(const Color&, float borderWidth, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix = TransformationMatrix());
-    virtual void drawTexture(const BitmapTexture&, const FloatRect&, const TransformationMatrix&, float opacity, const BitmapTexture* maskTexture);
-    virtual void drawTexture(uint32_t texture, Flags, const IntSize& textureSize, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, const BitmapTexture* maskTexture);
-    virtual void bindSurface(BitmapTexture* surface);
-    virtual void beginClip(const TransformationMatrix&, const FloatRect&);
-    virtual void beginPainting(PaintFlags = 0);
-    virtual void endPainting();
-    virtual void endClip();
-    virtual IntSize maxTextureSize() { return IntSize(2000, 2000); }
-    virtual PassRefPtr<BitmapTexture> createTexture();
-    virtual const char* type() const;
-    static PassOwnPtr<TextureMapperGL> create() { return adoptPtr(new TextureMapperGL); }
-    void setGraphicsContext(GraphicsContext* context) { m_context = context; }
-    GraphicsContext* graphicsContext() { return m_context; }
-    virtual bool isOpenGLBacked() const { return true; }
-    void platformUpdateContents(NativeImagePtr, const IntRect&, const IntRect&);
-    virtual AccelerationMode accelerationMode() const { return OpenGLMode; }
+    // TextureMapper implementation
+    virtual void drawBorder(const Color&, float borderWidth, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix = TransformationMatrix()) OVERRIDE;
+    virtual void drawTexture(const BitmapTexture&, const FloatRect&, const TransformationMatrix&, float opacity, const BitmapTexture* maskTexture) OVERRIDE;
+    virtual void drawTexture(uint32_t texture, Flags, const IntSize& textureSize, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, const BitmapTexture* maskTexture) OVERRIDE;
+    virtual void bindSurface(BitmapTexture* surface) OVERRIDE;
+    virtual void beginClip(const TransformationMatrix&, const FloatRect&) OVERRIDE;
+    virtual void beginPainting(PaintFlags = 0) OVERRIDE;
+    virtual void endPainting() OVERRIDE;
+    virtual void endClip() OVERRIDE;
+    virtual IntSize maxTextureSize() const OVERRIDE { return IntSize(2000, 2000); }
+    virtual PassRefPtr<BitmapTexture> createTexture() OVERRIDE;
+    virtual GraphicsContext* graphicsContext() OVERRIDE { return m_context; }
+    virtual AccelerationMode accelerationMode() const OVERRIDE { return OpenGLMode; }
+    virtual void setGraphicsContext(GraphicsContext* context) OVERRIDE { m_context = context; }
 
 #if ENABLE(CSS_FILTERS)
     void drawFiltered(const BitmapTexture& sourceTexture, const BitmapTexture& contentTexture, const FilterOperation&);

Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
@@ -393,11 +393,6 @@ void TextureMapperGL::drawTexture(uint32_t texture, Flags flags, const IntSize& 
     drawRect(targetRect, modelViewMatrix, shaderInfo.get(), GL_TRIANGLE_FAN, needsBlending);
 }
 
-const char* TextureMapperGL::type() const
-{
-    return "OpenGL";
-}
-
 bool BitmapTextureGL::canReuseWith(const IntSize& contentsSize, Flags)
 {
     return contentsSize == m_textureSize;

Source/WebCore/platform/graphics/texmap/TextureMapperImageBuffer.h
@@ -48,19 +48,21 @@ private:
 
 class TextureMapperImageBuffer : public TextureMapper {
 public:
-    virtual void drawBorder(const Color& color, float borderWidth, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix = TransformationMatrix()) { };
-    virtual void drawTexture(const BitmapTexture&, const FloatRect& targetRect, const TransformationMatrix&, float opacity, const BitmapTexture* maskTexture);
-    virtual void beginClip(const TransformationMatrix&, const FloatRect&);
-    virtual void bindSurface(BitmapTexture* surface) { m_currentSurface = surface;}
-    virtual void endClip() { graphicsContext()->restore(); }
     static PassOwnPtr<TextureMapper> create() { return adoptPtr(new TextureMapperImageBuffer); }
-    PassRefPtr<BitmapTexture> createTexture() { return BitmapTextureImageBuffer::create(); }
+
+    // TextureMapper implementation
+    virtual void drawBorder(const Color& color, float borderWidth, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix = TransformationMatrix()) OVERRIDE { };
+    virtual void drawTexture(const BitmapTexture&, const FloatRect& targetRect, const TransformationMatrix&, float opacity, const BitmapTexture* maskTexture) OVERRIDE;
+    virtual void beginClip(const TransformationMatrix&, const FloatRect&) OVERRIDE;
+    virtual void bindSurface(BitmapTexture* surface) OVERRIDE { m_currentSurface = surface;}
+    virtual void endClip() OVERRIDE { graphicsContext()->restore(); }
+    virtual PassRefPtr<BitmapTexture> createTexture() OVERRIDE { return BitmapTextureImageBuffer::create(); }
+    virtual AccelerationMode accelerationMode() const OVERRIDE { return SoftwareMode; }
+
     inline GraphicsContext* currentContext()
     {
         return m_currentSurface ? static_cast<BitmapTextureImageBuffer*>(m_currentSurface.get())->graphicsContext() : graphicsContext();
     }
-
-    virtual AccelerationMode accelerationMode() const { return SoftwareMode; }
 
 private:
     RefPtr<BitmapTexture> m_currentSurface;

commit 05386231e638875328a5588f17d5c7590c07c5dd

[Qt] TextureMapperGL::drawTexture marked OVERRIDE but does not override any member function. https://bugs.webkit.org/show_bug.cgi?id=86347
This bug was introduced by r116834.
TextureMapperGL implements two drawTexture functions.
The first one takes a BitmapTexture as an argument and does override TextureMapper::drawTexture().
The second one takes a texture id as an argument and does not override any member function.

* platform/graphics/texmap/TextureMapperGL.h:

git-svn-id: http://svn.webkit.org/repository/webkit/trunk@116931 268f45cc-cd09-0410-ab3c-d52691b4dbfc

Source/WebCore/platform/graphics/texmap/TextureMapperGL.h
@@ -51,7 +51,7 @@ public:
     // TextureMapper implementation
     virtual void drawBorder(const Color&, float borderWidth, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix = TransformationMatrix()) OVERRIDE;
     virtual void drawTexture(const BitmapTexture&, const FloatRect&, const TransformationMatrix&, float opacity, const BitmapTexture* maskTexture) OVERRIDE;
-    virtual void drawTexture(uint32_t texture, Flags, const IntSize& textureSize, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, const BitmapTexture* maskTexture) OVERRIDE;
+    virtual void drawTexture(uint32_t texture, Flags, const IntSize& textureSize, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, const BitmapTexture* maskTexture);
     virtual void bindSurface(BitmapTexture* surface) OVERRIDE;
     virtual void beginClip(const TransformationMatrix&, const FloatRect&) OVERRIDE;
     virtual void beginPainting(PaintFlags = 0) OVERRIDE;